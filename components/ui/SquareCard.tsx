efa8f496dcb8160ef498dde0e01731e518582816
Heyder Medina <medinaheyder@gmail.com>
2025-12-17 13:59:15 -0600

    refactor: card layout improved

--- components/ui/SquareCard.tsx
+++ components/ui/SquareCard.tsx
@@ -1,15 +1,39 @@
 interface SquareCardProps {
     title: string;
     content?: string;
+    image?: string;
 }
 
-export default function SquareCard({ title, content }: SquareCardProps) {
+export default function SquareCard({ title, content, image }: SquareCardProps) {
     return (
-        <div className="flex flex-col justify-center w-full h-full p-6
-            rounded-3xl ring ring-inset ring-white/10
-           bg-black/30 backdrop-blur-xl shadow-lg shadow-black/20">
-            <span className="text-2xl tracking-tight font-medium">{title}</span>
-            <p className="text-base">{content}</p>
+        <div
+            className="relative flex flex-col justify-start w-full h-full p-6 gap-2
+            rounded-3xl ring ring-inset ring-white/10 bg-black/30
+            backdrop-blur-xl shadow-lg shadow-black/20 overflow-hidden"
+        >
+            {image && (
+                <>
+                    <picture className="absolute inset-0 -z-10 w-full h-full">
+                        <img
+                            src={image}
+                            alt=""
+                            className="relative object-cover w-full h-full"
+                        />
+                    </picture>
+
+                    {/* <div className="absolute inset-0 -z-5 bg-black/50" /> */}
+                </>
+            )}
+
+            <h3 className="text-2xl tracking-tight font-medium">
+                {title}
+            </h3>
+
+            {content && (
+                <p className="text-base">
+                    {content}
+                </p>
+            )}
         </div>
     );
 }
