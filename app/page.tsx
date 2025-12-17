f7f64bd613affdc5ef158c3d5dc696196f05e54c
Heyder Medina <medinaheyder@gmail.com>
2025-12-17 16:18:28 -0600

    refactor: home layout improved adding the long card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import { getLatestLaunch } from '@/lib/api/spacex';
+
 import Header from '@/components/layout/Header';
 import Sidebar from '@/components/layout/Sidebar';
 import PanelRecentLaunches from '@/components/layout/PanelRecentLaunches';
 import Banner from '@/components/ui/Banner';
 import SquareCard from '@/components/ui/SquareCard';
+import LongCard from '@/components/ui/LongCard';
 
-export default function Home() {
+export default async function Home() {
+  const launch = await getLatestLaunch();
   return (
     <div className="relative flex flex-col h-screen w-full">
       <Header />
@@ -17,23 +21,23 @@ export default function Home() {
         />
       </picture>
 
-
       <div className="grid grid-cols-[350px_1fr] gap-2 flex-1 p-2">
 
+        {/* left side */}
         <section className='grid grid-rows-[150px_1fr] gap-2'>
-          {/* Sidebar */}
           <Sidebar />
-          <SquareCard title="Latest Launch" content="Here is some info about the latest launch"
-           image="/test.png" />
+          <LongCard 
+            title="Latest Launch"
+            launchName={launch.name}
+            id={launch.id}
+            image="/test.png" />
         </section>
-
 
         {/* Main content */}
         <main className="grid grid-rows-[200px_1fr] gap-2">
           {/* General info */}
           <section className="grid grid-cols-[42em_1fr] gap-2">
             <Banner />
-
             <div className="flex gap-2">
               <SquareCard title="Succesfull Launches" content="Contenido de la tarjeta 1" />
               <SquareCard title="Failed Launches" content="Contenido de la tarjeta 2" />
@@ -45,7 +49,6 @@ export default function Home() {
             <PanelRecentLaunches />
             <SquareCard title="Upcoming Launches" content="Some info about upcoming launches" />
           </div>
-
         </main>
       </div>
     </div>
