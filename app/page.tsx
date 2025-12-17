7a13fcfa199b306e1c3dc899a3b27777c5cd16d2
Heyder Medina <medinaheyder@gmail.com>
2025-12-17 13:56:12 -0600

    refactor: home layout improved

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
-import { Clock } from 'lucide-react';
-import { ChevronRight } from 'lucide-react';
-
 import Header from '@/components/layout/Header';
 import Sidebar from '@/components/layout/Sidebar';
+import PanelRecentLaunches from '@/components/layout/PanelRecentLaunches';
 import Banner from '@/components/ui/Banner';
 import SquareCard from '@/components/ui/SquareCard';
-import LargeCard from '@/components/ui/LargeCard';
-import PrimaryButton from '@/components/ui/PrimaryButton';
-
-const glassCard =
-  'rounded-3xl ring ring-inset ring-white/10 bg-black/30 backdrop-blur-xl shadow-lg shadow-black/20';
 
 export default function Home() {
   return (
@@ -30,7 +23,8 @@ export default function Home() {
         <section className='grid grid-rows-[150px_1fr] gap-2'>
           {/* Sidebar */}
           <Sidebar />
-          <SquareCard title="Quick Info" content="Some quick info here" />
+          <SquareCard title="Latest Launch" content="Here is some info about the latest launch"
+           image="/test.png" />
         </section>
 
 
@@ -46,25 +40,12 @@ export default function Home() {
             </div>
           </section>
 
-          {/* Recent launches */}
-          <section className={`flex flex-col gap-4 p-6 ${glassCard}`}>
-            <header className="flex items-center gap-2">
-              <span className={`flex size-12 items-center justify-center rounded-full bg-white/10 ${glassCard}`}>
-                <Clock className="w-6 h-6 text-white/80" />
-              </span>
-              <h2 className="text-3xl tracking-tight font-medium">
-                Recent Launches
-              </h2>
-            </header>
+          <div className='grid grid-cols-[2fr_1fr] gap-2'>
+            {/* Recent launches */}
+            <PanelRecentLaunches />
+            <SquareCard title="Upcoming Launches" content="Some info about upcoming launches" />
+          </div>
 
-            <div className="flex flex-col gap-2 flex-1">
-              <LargeCard />
-              <LargeCard />
-              <LargeCard />
-
-              <PrimaryButton text="See All" Icon={ChevronRight} />
-            </div>
-          </section>
         </main>
       </div>
     </div>
