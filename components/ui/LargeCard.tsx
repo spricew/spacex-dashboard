f916841db10e69e2a9213afc8e96a76e2c14d4de
Heyder Medina <medinaheyder@gmail.com>
2025-12-16 22:11:59 -0600

    feat: add LargeCard component

---
+++ components/ui/LargeCard.tsx
@@ -0,0 +1,12 @@
+
+
+export default function LargeCard() {
+    return (
+        <div className="p-6
+            rounded-2xl ring ring-inset ring-white/10
+           bg-black/30 backdrop-blur-xl">
+
+            Large Card Component
+        </div>
+    );
+}
