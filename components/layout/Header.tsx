3603bee0ad8d8d75cdc879aaa1440ad2e609ba48
Heyder Medina <medinaheyder@gmail.com>
2025-12-16 20:12:05 -0600

    feat: add Header component with spacex logo

---
+++ components/layout/Header.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center px-12 py-6
+      bg-black/10 backdrop-blur-xl brightness-150 shadow-lg">
+
+      <Image
+        src="/SpaceX-White-Logo.svg"
+        alt="SpaceX Logo"
+        className="object-cover w-40 h-10"
+        width={128}
+        height={32}
+        priority
+      />
+
+      <h1 className="text-2xl tracking-tighter font-medium">
+        SpaceX Dashboard
+      </h1>
+    </header>
+  );
+}
