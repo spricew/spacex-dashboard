09fad16fee683a43003523824b800d0a85f141d5
Heyder Medina <medinaheyder@gmail.com>
2025-12-17 12:03:56 -0600

    feat: add list item component and added icons

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,14 +1,15 @@
-import Link from 'next/link';
+import ListItem from "@/components/ui/ListItem";
+import { Home } from "lucide-react";
+import { Rocket } from "lucide-react";
 
 export default function Sidebar() {
     return (
-        <aside className="flex flex-col p-6 rounded-3xl ring ring-inset ring-white/10 bg-black/30 backdrop-blur-xl shadow-lg shadow-black/20">
-            <h2 className="mb-4 font-semibold">Dashboard</h2>
+        <aside className="flex flex-col p-6 rounded-3xl justify-center
+        ring ring-inset ring-white/10 bg-black/30 backdrop-blur-xl shadow-lg shadow-black/20">
             <nav>
-                <ul className="space-y-2">
-                    <li><Link href="/launches">Launches</Link></li>
-                    <li><Link href="/rockets">Rockets</Link></li>
-                    <li><Link href="/about">About</Link></li>
+                <ul className="space-y-4">
+                    <ListItem href="/" label="Home" Icon={Home} />
+                    <ListItem href="/launches" label="Launches" Icon={Rocket} />
                 </ul>
             </nav>
         </aside>
