3b56edbabe63c1383befdd396e15d5b09fdf63cd
Heyder Medina <medinaheyder@gmail.com>
2025-12-17 12:11:54 -0600

    refactor: card style refactorized

--- components/ui/SquareCard.tsx
+++ components/ui/SquareCard.tsx
@@ -7,8 +7,8 @@ export default function SquareCard({ title, content }: SquareCardProps) {
     return (
         <div className="flex flex-col justify-center w-full h-full p-6
             rounded-3xl ring ring-inset ring-white/10
-           bg-white/10 backdrop-blur-xl shadow-lg shadow-black/20">
-            <span className="text-2xl font-medium">{title}</span>
+           bg-black/30 backdrop-blur-xl shadow-lg shadow-black/20">
+            <span className="text-2xl tracking-tight font-medium">{title}</span>
             <p className="text-base">{content}</p>
         </div>
     );
