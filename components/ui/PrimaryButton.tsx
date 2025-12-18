b94d427668dfb3f90ac81df0ead3db73fd24e3df
Heyder Medina <medinaheyder@gmail.com>
2025-12-17 21:03:47 -0600

    feat: added extra class attribute

--- components/ui/PrimaryButton.tsx
+++ components/ui/PrimaryButton.tsx
@@ -5,15 +5,16 @@ interface PrimaryButtonProps {
     Icon: ElementType;
     iconClass?: string;
     textClass?: string;
+    extraClass?: string;
     gap?: string;
 }
 
-export default function PrimaryButton({ text, Icon, iconClass, gap="gap-2", textClass }: PrimaryButtonProps) {
+export default function PrimaryButton({ text, Icon, iconClass, gap="gap-2", textClass, extraClass }: PrimaryButtonProps) {
     return (
         <button
             className={`flex items-center w-fit px-4 py-2 ${gap}
-            rounded-full ring ring-inset ring-white/20 bg-primary-base
-            cursor-pointer`}
+            rounded-full ring ring-inset ring-white/20 bg-primary-base tracking-tight font-medium
+            cursor-pointer ${extraClass}`}
         >
             <span className={textClass}>{text}</span>
             <Icon className={`size-5 ml-2 ${iconClass}`}/>
