1f9261a538cd68e4c797fadacb7064c11f427d14
Heyder Medina <medinaheyder@gmail.com>
2025-12-17 20:42:31 -0600

    feat: extra classes added to button layout

--- components/ui/PrimaryButton.tsx
+++ components/ui/PrimaryButton.tsx
@@ -3,17 +3,20 @@ import { ElementType } from "react";
 interface PrimaryButtonProps {
     text: string;
     Icon: ElementType;
+    iconClass?: string;
+    textClass?: string;
+    gap?: string;
 }
 
-export default function PrimaryButton({ text, Icon }: PrimaryButtonProps) {
+export default function PrimaryButton({ text, Icon, iconClass, gap="gap-2", textClass }: PrimaryButtonProps) {
     return (
         <button
-            className="flex items-center w-fit px-4 py-2
+            className={`flex items-center w-fit px-4 py-2 ${gap}
             rounded-full ring ring-inset ring-white/20 bg-primary-base
-            cursor-pointer"
+            cursor-pointer`}
         >
-            {text}
-            <Icon className="size-5" />
+            <span className={textClass}>{text}</span>
+            <Icon className={`size-5 ml-2 ${iconClass}`}/>
         </button>
     );
 }
