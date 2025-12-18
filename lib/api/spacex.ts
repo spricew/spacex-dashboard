7844156e430da03c038b9b64b9e6a81f083b48e9
Heyder Medina <medinaheyder@gmail.com>
2025-12-18 09:13:34 -0600

    feat: add getRecentLaunches function to fetch recent SpaceX launches

--- lib/api/spacex.ts
+++ lib/api/spacex.ts
@@ -13,11 +13,43 @@ export async function getLaunches() {
     return data;
 }
 
+export async function getRecentLaunches() {
+    const res = await fetch(
+        `${BASE_URL}/launches/query`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                query: {
+                    upcoming: false,
+                },
+                options: {
+                    sort: {
+                        date_utc: 'desc',
+                    },
+                    limit: 2,
+                },
+            }),
+        }
+    );
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch recent launches');
+    }
+
+    const data = await res.json();
+    const launches = data.docs;
+
+    return launches;
+}
+
 export async function getLatestLaunch() {
     const response = await fetch(`${BASE_URL}/launches/latest`);
 
     if (!response.ok) {
-        throw new Error('Failed to fetch launches');
+        throw new Error('Failed to fetch latest launches');
     }
 
     const data = await response.json();
