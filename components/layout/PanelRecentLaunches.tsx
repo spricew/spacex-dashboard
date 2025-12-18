2869fabaac46643582041fedc5d5b612e72e53cc
Heyder Medina <medinaheyder@gmail.com>
2025-12-18 09:13:59 -0600

    feat: update PanelRecentLaunches to fetch and display recent launches from SpaceX API

--- components/layout/PanelRecentLaunches.tsx
+++ components/layout/PanelRecentLaunches.tsx
@@ -1,33 +1,48 @@
+import { getRecentLaunches } from '@/lib/api/spacex';
+
 import { Clock } from 'lucide-react';
 import { ChevronRight } from 'lucide-react';
 
 import RowCard from '@/components/ui/RowCard';
 import PrimaryButton from '@/components/ui/PrimaryButton';
 
-export default function PanelRecentLaunches() {
-    const glassCard =
-        'rounded-3xl ring ring-inset ring-white/10 bg-black/30 backdrop-blur-xl shadow-lg shadow-black/20';
-        
-    return (
-        <section className={`flex flex-col gap-4 p-6 ${glassCard}`}>
-            <header className="flex items-center gap-2">
-                <span className={`flex size-12 items-center justify-center rounded-full ${glassCard}`}>
-                    <Clock className="w-6 h-6 text-white/80" />
-                </span>
-                <h2 className="text-3xl tracking-tight font-medium">
-                    Recent Launches
-                </h2>
-            </header>
+export default async function PanelRecentLaunches() {
+  const launches = await getRecentLaunches();
 
-            <div className="flex flex-col gap-2 flex-1">
-                <RowCard />
-                <RowCard />
-                <RowCard />
+  const glassCard =
+    'rounded-3xl ring ring-inset ring-white/10 bg-black/30 backdrop-blur-xl shadow-lg shadow-black/20';
 
-                <PrimaryButton text="See All" 
-                Icon={ChevronRight} 
-                iconClass="-mr-2"
-                textClass="-mr-3"/>
-            </div>
-        </section>);
+  return (
+    <section className={`flex flex-col gap-4 p-6 ${glassCard}`}>
+      <header className="flex items-center gap-2">
+        <span className={`flex size-12 items-center justify-center rounded-full ${glassCard}`}>
+          <Clock className="w-6 h-6 text-white/80" />
+        </span>
+        <h2 className="text-3xl tracking-tight font-medium">
+          Recent Launches
+        </h2>
+      </header>
+
+      <div className="flex flex-col gap-2 flex-1">
+        {launches.map((launch) => (
+          <RowCard
+            key={launch.id}
+            launchName={launch.name}
+            id={launch.id}
+            patch={launch.links.patch.small}
+            launchDate={launch.date_utc}
+            successStatus={launch.success}
+            // patch={launch.links?.patch?.small}
+          />
+        ))}
+
+        <PrimaryButton
+          text="See All"
+          Icon={ChevronRight}
+          iconClass="-mr-2"
+          textClass="-mr-3"
+        />
+      </div>
+    </section>
+  );
 }
