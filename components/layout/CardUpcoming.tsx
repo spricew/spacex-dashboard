7389dff21741db1929f1885f24b31438415adf21
Heyder Medina <medinaheyder@gmail.com>
2025-12-18 20:55:59 -0600

    feat: update CardUpcoming component to display next launch details using LongCard

--- components/layout/CardUpcoming.tsx
+++ components/layout/CardUpcoming.tsx
@@ -1,8 +1,19 @@
 import { getUpcomingLaunch } from "@/lib/api/spacex"
-import SquareCard from "@/components/ui/SquareCard";
+import LongCard from "@/components/ui/LongCard";
 
 export default async function CardUpcoming() {
+    const upcoming = await getUpcomingLaunch();
     return (
-        <SquareCard title="Upcoming Launches" content="Some info about upcoming launches" />
+        <LongCard
+            title="Next Launch"
+            launchName={upcoming.name}
+            id={upcoming.id}
+            patch={upcoming.links.patch.small}
+            launchDate={upcoming.date_utc}
+            successStatus={upcoming.success}
+            isUpcoming={upcoming.upcoming}
+            details={upcoming.details}
+            // bgImage="/test3.png"
+        />
     );
 }
