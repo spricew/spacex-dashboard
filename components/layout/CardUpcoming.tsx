740388bf1c58ab7b51056950ecc25aa791ea1fbb
Heyder Medina <medinaheyder@gmail.com>
2025-12-19 13:15:58 -0600

    feat: enhance CardUpcoming to display rocket details and update launch information

--- components/layout/CardUpcoming.tsx
+++ components/layout/CardUpcoming.tsx
@@ -1,19 +1,24 @@
-import { getUpcomingLaunch } from "@/lib/api/spacex"
+import { getRocketById } from "@/lib/api/spacex";
+import { getUpcomingLaunch } from "@/lib/api/spacex";
 import LongCard from "@/components/ui/LongCard";
 
 export default async function CardUpcoming() {
-    const upcoming = await getUpcomingLaunch();
+    const launch = await getUpcomingLaunch();
+    const rocket = await getRocketById(launch.rocket);
+
     return (
         <LongCard
             title="Next Launch"
-            launchName={upcoming.name}
-            id={upcoming.id}
-            patch={upcoming.links.patch.small}
-            launchDate={upcoming.date_utc}
-            successStatus={upcoming.success}
-            isUpcoming={upcoming.upcoming}
-            details={upcoming.details}
-            // bgImage="/test3.png"
+            launchName={launch.name}
+            id={launch.id}
+            patch={launch.links.patch.small}
+            launchDate={launch.date_utc}
+            successStatus={launch.success}
+            isUpcoming={launch.upcoming}
+            // details={launch.details}
+details="Adipisicing velit adipisicing irure laboris occaecat consequat qui. Sint enim cillum ea cillum. Ullamco deserunt anim enim excepteur id nulla do anim officia eu tempor nisi. Deserunt et dolore reprehenderit dolore aliquip consectetur sunt commodo est anim. Cillum elit mollit enim excepteur commodo non sint nostrud aute nisi sint quis. Veniam sint et est sit pariatur."
+            rocket={rocket.name}
+            bgImage="/test.png"
         />
     );
 }
