41c05cbd2e64c1ef988c4ec120452c3dad971242
Heyder Medina <medinaheyder@gmail.com>
2025-12-20 17:22:18 -0600

    refactor: update layout of Home component to improve structure and responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         />
       </picture>
 
-      <div className="grid grid-cols-[350px_1fr] gap-2 flex-1 p-2">
+      <div className="grid grid-cols-[350px_2fr_1fr] gap-2 flex-1 p-2">
 
         {/* left side */}
         <section className='grid grid-rows-[130px_1fr] gap-2'>
@@ -28,24 +28,14 @@ export default function Home() {
         </section>
 
         {/* Main content */}
-        <main className="grid grid-rows-[200px_1fr] gap-2">
-          {/* General info */}
-          <section className="grid grid-cols-[42em_1fr] gap-2">
-            <Banner />
-            <div className="flex gap-2">
-              <SquareCard title="Succesfull Launches" content="Contenido de la tarjeta 1" />
-              <SquareCard title="Failed Launches" content="Contenido de la tarjeta 2" />
-            </div>
-          </section>
-
-          <div className='grid grid-cols-[2fr_1fr] gap-2'>
-            {/* Recent launches */}
-            <CardRecents />
-
-            {/* Upcoming launch */}
-            <CardUpcoming />
-          </div>
+        <main className="flex gap-2">
+          {/* Recent launches */}
+          <CardRecents />
         </main>
+        <div className='grid grid-rows-[2fr_1fr] gap-2'>
+          {/* Upcoming launch */}
+          <CardUpcoming />
+        </div>
       </div>
     </div>
   );
