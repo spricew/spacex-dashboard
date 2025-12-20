c3c589a1d0c4858290d24e5a275e391aef1ead5c
Heyder Medina <medinaheyder@gmail.com>
2025-12-19 22:20:32 -0600

    feat: wrap SpaceX logo in a Link component for improved navigation

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Header() {
@@ -5,14 +6,16 @@ export default function Header() {
     <header className="flex justify-between items-center px-12 py-6
       bg-black/10 backdrop-blur-xl brightness-150 shadow-lg">
 
-      <Image
-        src="/SpaceX-White-Logo.svg"
-        alt="SpaceX Logo"
-        className="object-cover w-40 h-10"
-        width={128}
-        height={32}
-        priority
-      />
+      <Link href={"/"} >
+        <Image
+          src="/SpaceX-White-Logo.svg"
+          alt="SpaceX Logo"
+          className="object-cover w-40 h-10"
+          width={128}
+          height={32}
+          priority
+        />
+      </Link>
     </header>
   );
 }
