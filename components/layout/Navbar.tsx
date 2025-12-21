2ab0d4482d90345f245087bcbbab667701204c79
Heyder Medina <medinaheyder@gmail.com>
2025-12-21 16:18:44 -0600

    refactor: update Navbar component padding and logo height for improved layout consistency

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center px-12 py-6
+    <header className="flex justify-between items-center px-4 md:px-12 py-6
       bg-primary-800 shadow-lg">
 
       <Link href={"/"} className="transition ease-out duration-300 hover:scale-105">
@@ -12,7 +12,7 @@ export default function Header() {
           alt="SpaceX Logo"
           className="object-cover w-40 h-10"
           width={128}
-          height={32}
+          height={128}
           priority
         />
       </Link>
