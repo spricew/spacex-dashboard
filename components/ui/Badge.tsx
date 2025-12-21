c702fc9aa1c651d3423552922257a186c6ac6fca
Heyder Medina <medinaheyder@gmail.com>
2025-12-21 14:12:50 -0600

    feat: add extraClass prop to Badge component for customizable styling

--- components/ui/Badge.tsx
+++ components/ui/Badge.tsx
@@ -1,9 +1,10 @@
 interface BadgeProps {
     status: boolean;
     UpcomingState?: boolean;
+    extraClass?: string;
 }
 
-export default function Badge({ status, UpcomingState }: BadgeProps) {
+export default function Badge({ status, UpcomingState, extraClass }: BadgeProps) {
     let statusStyles = status
         ? {
             badge: 'text-green-400 bg-green-900 ring-green-400/20',
@@ -26,7 +27,8 @@ export default function Badge({ status, UpcomingState }: BadgeProps) {
 
     return (
         <span
-            className={`flex items-center w-fit px-1.5 py-0.5 text-xs font-medium rounded-full ring ring-inset ${statusStyles.badge}`}
+            className={`flex items-center h-fit w-fit px-1.5 py-0.5 text-xs font-medium rounded-full ring ring-inset 
+            ${extraClass} ${statusStyles.badge}`}
         >
             <span
                 className={`mr-1 inline-block size-1.5 rounded-full ${statusStyles.dot}`}
