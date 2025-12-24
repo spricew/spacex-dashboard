e73e46d514f8faca7a466279d592c7e0c28046b7
Heyder Medina <medinaheyder@gmail.com>
2025-12-24 12:24:38 -0600

    refactor: streamline launch fetching logic and enhance getRecentLaunches function

--- lib/api/spacex.ts
+++ lib/api/spacex.ts
@@ -7,7 +7,7 @@ export async function getLaunchById(id: string) {
     if (!response.ok) {
         throw new Error('Failed to fetch launches');
     }
-    
+
     const data = await response.json();
     return data;
 }
@@ -17,61 +17,41 @@ export async function getLaunches({
     page = 1,
     limit = 12,
     order = 'desc',
-  }: {
+}: {
     page?: number;
     limit?: number;
     order?: 'asc' | 'desc';
-  }) {
+}) {
     const response = await fetch(`${BASE_URL}/launches/query`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        options: {
-          page,
-          limit,
-          sort: {
-            date_utc: order,
-          },
-        },
-      }),
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            options: {
+                page,
+                limit,
+                sort: {
+                    date_utc: order,
+                },
+            },
+        }),
     });
-  
+
     if (!response.ok) {
-      throw new Error('Failed to fetch launches');
+        throw new Error('Failed to fetch launches');
     }
-  
+
     const data = await response.json();
     return data;
-  }
-  
-// get recent launches (with limit)
+}
+
+// reutiliza la logica de getlaunches con diferentes parametros
 export async function getRecentLaunches() {
-    const res = await fetch(
-        `${BASE_URL}/launches/query`,
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                options: {
-                    sort: {
-                        date_utc: 'desc',
-                    },
-                    limit: 10,
-                },
-            }),
-        }
-    );
-
-    if (!res.ok) {
-        throw new Error('Failed to fetch recent launches');
-    }
-
-    const data = await res.json();
-    const launches = data.docs;
-
-    return launches;
+    const data = await getLaunches({
+        limit: 10,
+        order: 'desc',
+        page: 1
+    });
+    return data.docs;
 }
 
 // get the latest launch
