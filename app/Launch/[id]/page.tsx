c423f14470ec0a8b2bb161db2b7d82a71f8c9900
Heyder Medina <medinaheyder@gmail.com>
2025-12-24 15:26:04 -0600

    refactor: grid layout removed

--- app/Launch/[id]/page.tsx
+++ app/Launch/[id]/page.tsx
@@ -24,116 +24,119 @@ export default async function LaunchPage({ params }: LaunchPageProps) {
     return (
         <div className="flex flex-col min-h-screen w-full">
             <Navbar />
-            <div className="flex flex-col gap-4 px-24 py-18">
-                <div className="grid grid-cols-[1fr_25rem] min-h-fit h-100 gap-2">
+            <div className="flex flex-col gap-8 px-24 py-18">
 
-                    {/* informacion */}
-                    <div className={`flex flex-col gap-4 p-8 rounded-3xl bg-primary-300`}>
-                        <header className="flex items-center gap-4">
+                {/* informacion */}
+                <div className={`flex flex-col gap-4 p-8 rounded-3xl bg-primary-300`}>
+                    <header className="flex items-center gap-4">
 
-                            {/* icono */}
-                            {launch.links.patch.small && (
-                                <img
-                                    src={launch.links.patch.small}
-                                    alt="Mission patch"
-                                    className="size-16 object-contain"
-                                />
+                        {/* icono */}
+                        {launch.links.patch.small && (
+                            <img
+                                src={launch.links.patch.small}
+                                alt="Mission patch"
+                                className="size-16 object-contain"
+                            />
+                        )}
+
+                        <div>
+                            {/* nombre del launch */}
+                            <h1 className="text-3xl font-semibold tracking-tight">
+                                {launch.name}
+                            </h1>
+                            {/* identificador launch */}
+                            <span className="flex gap-1 text-sm text-white/50">
+                                <KeyRound className="size-4" />
+                                {launch.id}
+                            </span>
+                            {/* numero de vuelo */}
+                            <p className="text-sm text-white/60">
+                                Flight #{launch.flight_number}
+                            </p>
+                        </div>
+                    </header>
+
+                    <section className="flex flex-wrap gap-6 text-sm">
+                        {/* fecha */}
+                        <div>
+                            <span className="flex gap-1 text-white/50">
+                                <Calendar className="size-4" />
+                                Date
+                            </span>
+                            <span>{formatDate(launch.date_utc)}</span>
+                        </div>
+
+                        {/* status */}
+                        <div>
+                            <span className="flex gap-1 text-white/50">
+                                <CircleCheck className="size-4" />
+                                Status
+                            </span>
+                            <Badge status={launch.success} />
+                        </div>
+
+                        {/* cohete */}
+                        <div>
+                            <span className="flex gap-1 text-white/50">
+                                <Rocket className="size-4" />
+                                Rocket
+                            </span>
+                            <span>{rocket.name}</span>
+                        </div>
+                    </section>
+
+                    {/* Detalles */}
+                    <section className="max-w-2xl">
+                        <h2 className="text-xl font-medium mb-1">Mission Details</h2>
+                        {launch.details ? (
+                            <p className="text-sm leading-tight text-pretty">
+                                {launch.details}
+                            </p>
+                        ) :
+                            (<div className="flex items-center gap-2 p-4 bg-white/10 rounded-xl">
+                                <CircleQuestionMark className="size-5" />
+                                <p className="text-sm">
+                                    Classified… or maybe just unknown.
+                                </p>
+                            </div>
                             )}
 
-                            <div>
-                                {/* nombre del launch */}
-                                <h1 className="text-3xl font-semibold tracking-tight">
-                                    {launch.name}
-                                </h1>
-                                {/* identificador launch */}
-                                <span className="flex gap-1 text-sm text-white/50">
-                                    <KeyRound className="size-4" />
-                                    {launch.id}
-                                </span>
-                                {/* numero de vuelo */}
-                                <p className="text-sm text-white/60">
-                                    Flight #{launch.flight_number}
-                                </p>
-                            </div>
-                        </header>
-
-                        <section className="flex flex-wrap gap-6 text-sm">
-                            {/* fecha */}
-                            <div>
-                                <span className="flex gap-1 text-white/50">
-                                    <Calendar className="size-4" />
-                                    Date
-                                </span>
-                                <span>{formatDate(launch.date_utc)}</span>
-                            </div>
-
-                            {/* status */}
-                            <div>
-                                <span className="flex gap-1 text-white/50">
-                                    <CircleCheck className="size-4" />
-                                    Status
-                                </span>
-                                <Badge status={launch.success} />
-                            </div>
-
-                            {/* cohete */}
-                            <div>
-                                <span className="flex gap-1 text-white/50">
-                                    <Rocket className="size-4" />
-                                    Rocket
-                                </span>
-                                <span>{rocket.name}</span>
-                            </div>
-                        </section>
-
-                        {/* Detalles */}
-                        <section className="max-w-2xl">
-                            <h2 className="text-xl font-medium mb-1">Mission Details</h2>
-                            {launch.details ? (
-                                <p className="text-sm leading-tight text-pretty">
-                                    {launch.details}
-                                </p>
-                            ) :
-                                (<div className="flex items-center gap-2 p-4 bg-white/10 rounded-xl">
-                                    <CircleQuestionMark className="size-5" />
-                                    <p className="text-sm">
-                                        Classified… or maybe just unknown.
-                                    </p>
-                                </div>
-                                )}
-
-                            <p className="mt-4 text-base">
-                                {/* &nbsp; Adds a single fixed space */}
-                                for more information, you can read the&nbsp;
-                                <a href={launch.links.article} target="_blank" rel="noopener noreferrer"
-                                    className="cursor-pointer group">
-                                    <span className="inline-flex gap-0.5 font-medium text-secondary-400
+                        <p className="mt-4 text-base">
+                            {/* &nbsp; Adds a single fixed space */}
+                            for more information, you can read the&nbsp;
+                            <a href={launch.links.article} target="_blank" rel="noopener noreferrer"
+                                className="cursor-pointer group">
+                                <span className="inline-flex gap-0.5 font-medium text-secondary-400
                                 group-hover:underline group-hover:underline-offset-2 group-hover:decoration-secondary-400">
-                                        official article
-                                        <ArrowUpRight strokeWidth={2.5} className="size-5 relative top-0.5" />
-                                    </span>
-                                </a>
-                            </p>
-                        </section>
+                                    official article
+                                    <ArrowUpRight strokeWidth={2.5} className="size-5 relative top-0.5" />
+                                </span>
+                            </a>
+                        </p>
+                    </section>
+
+                    <div className="flex flex-col gap-6 max-w-2xl">
+                        {/* Video */}
+                        {launch.links.webcast && (
+                            <VideoSection url={embedUrl} />
+                        )}
+
+                        {/* images carousel */}
+                        {imagesUrl.length > 0 &&
+                            (
+                                <div className="flex flex-col gap-2">
+
+                                    <h2 className="text-2xl font-medium mb-2">Launch Video</h2>
+                                    <Carousel images={imagesUrl} />
+                                </div>
+                            )
+                        }
                     </div>
 
-                    {/* launchpad */}
-                    <div className="flex flex-col p-6 bg-primary-300 rounded-3xl">
-
-                    </div>
                 </div>
 
-                {/* Video */}
-                {launch.links.webcast && (
-                    <VideoSection url={embedUrl} />
-                )}
 
-                {/* images carousel */}
-                {imagesUrl.length > 0 &&
-                    (
-                        <Carousel images={imagesUrl} />
-                    )
-                }
+
 
             </div>
         </div>
