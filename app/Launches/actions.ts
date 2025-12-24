a2c16f6dddf435b7b9c886ee9b2a176bbc3c9ba6
Heyder Medina <medinaheyder@gmail.com>
2025-12-24 17:03:31 -0600

    feat: enhance fetchLaunches function to support sorting by order

--- app/Launches/actions.ts
+++ app/Launches/actions.ts
@@ -2,8 +2,8 @@
 
 import { getLaunches, getRocketById } from "@/lib/api/spacex";
 
-export async function fetchLaunches(page: number) {
-  const launches = await getLaunches({ page, limit: 20 });
+export async function fetchLaunches(page: number, order?: 'asc' | 'desc') {
+  const launches = await getLaunches({ order, page, limit: 20 });
 
   const launchesWithRocket = await Promise.all(
     launches.docs.map(async (launch:any) => {
