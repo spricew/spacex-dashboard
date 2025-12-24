865a7f66f4667ca38c01069cc10815872a135376
Heyder Medina <medinaheyder@gmail.com>
2025-12-23 22:51:12 -0600

    refactor: enhance ListItem component with hover effects and adjust icon styling

--- components/ui/ListItem.tsx
+++ components/ui/ListItem.tsx
@@ -9,8 +9,9 @@ interface ListItemProps {
 
 export default function ListItem({ href, label, Icon }: ListItemProps) {
     return (
-        <li className='flex gap-4 items-center text-lg font-medium tracking-wide'>
-            <Icon className="size-6 text-white/80" />
+        <li className='flex gap-4 items-center text-lg font-medium tracking-wide
+        transition duration-300 ease-out hover:scale-102 hover:text-secondary-400'>
+            <Icon className="size-6" />
             <Link href={href}>{label}</Link>
         </li>
     );
