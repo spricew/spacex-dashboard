78e1607dce0829c21efd6287c28c67a885c226f9
Heyder Medina <medinaheyder@gmail.com>
2026-09-02 23:33:42 -0600

    feat: card skeleton base creation

---
+++ components/ui/Skeletons/CardSkeleton.tsx
@@ -0,0 +1,10 @@
+
+
+export function CardSkeleton() {
+    return(
+        <div className={`scale-up relative flex flex-col flex-1 gap-4 min-w-72 w-80 h-100 p-6 overflow-hidden
+        rounded-3xl ring ring-inset ring-white/10 bg-black/30 backdrop-blur-xl shadow-lg shadow-black/20`}>
+            
+        </div>
+    );
+}
